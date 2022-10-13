5c8c4564b6eedee8b1f06e012eafd466e64cb8c0
Samuel <sam.motsoane@gmail.com>
2022-10-13 14:15:01 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
-/**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
-
+#include<stdio.h>
+/*                                         
+ * Description: main- Printsout a string to stdout
+ * Return : 0 if success
+ */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+	return(0);
 }
