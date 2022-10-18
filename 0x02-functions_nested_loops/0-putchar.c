41a23ffda8d595468388865ecfdf6ded8d17c716
Samuel <sam.motsoane@gmail.com>
2022-10-18 22:12:19 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *main - prints _putchar, followed by a new line.
 *
@@ -15,7 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-
-	return (0);
-
+return (0);
 }
